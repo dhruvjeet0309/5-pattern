a730639171d8bbeb5c704a30aed26c193a09a190
dhruvjeet0309 <127146825+dhruvjeet0309@users.noreply.github.com>
2023-03-22 19:02:02 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+
+    for(i=1;i<=5;i++){
+        for(j=1;j<=9;j++){
+            if(i==2&&j==5){
+                cout<<"  ";
+                continue;
+            }
+            if(i==3&&j==4){
+                cout<<"  ";
+                continue;
+            }
+            if(i==3&&j==5){
+                cout<<"  ";
+                continue;
+            }
+            if(i==3&&j==6){
+                cout<<"  ";
+                continue;
+            }
+            if(i==4&&j==3){
+                cout<<"  ";
+                continue;
+            }
+            if(i==4&&j==4){
+                cout<<"  ";
+                continue;
+            }
+            if(i==4&&j==5){
+                cout<<"  ";
+                continue;
+            }
+            if(i==4&&j==6){
+                cout<<"  ";
+                continue;
+            }
+            if(i==4&&j==7){
+                cout<<"  ";
+                continue;
+            }
+            if(i==5&&j==2){
+                cout<<"  ";
+                continue;
+            }
+            if(i==5&&j==3){
+                cout<<"  ";
+                continue;
+            }
+            if(i==5&&j==4){
+                cout<<"  ";
+                continue;
+            }
+            if(i==5&&j==5){
+                cout<<"  ";
+                continue;
+            }
+            if(i==5&&j==6){
+                cout<<"  ";
+                continue;
+            }
+            if(i==5&&j==7){
+                cout<<"  ";
+                continue;
+            }
+            if(i==5&&j==8){
+                cout<<"  ";
+                continue;
+            }
+            cout<<"5 ";
+        }
+        cout<<"\n";
+    }
+}
